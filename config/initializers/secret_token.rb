214b8def53748ce1af4066d82a63598c4727d38f
Javier Velazquez <javier.velazquez@crowdint.com>
2011-11-28 17:22:22 -0600

    first commit for optativa project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optativa::Application.config.secret_token = 'fda85b53b2337fd70006f820891a232b24cf62f7825a70b5944397332563dc0115df4d3878c6100aa30e9c380788d8fc7bdb35a567d25dd042d0449f0fac7be6'
